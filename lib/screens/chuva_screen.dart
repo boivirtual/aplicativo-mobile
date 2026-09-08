afec9099fc73b8f9f02fe0ec553278d9fc72d141
boivirtual <boivirtual@agrolandes.com.br>
2026-09-08 07:00:38 -0300

    Ajuste: lib/screens/chuva_screen.dart

--- lib/screens/chuva_screen.dart
+++ lib/screens/chuva_screen.dart
@@ -274,7 +274,7 @@ class _ChuvaScreenState extends State<ChuvaScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFF5F5F5),
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text(
           'Chuva',

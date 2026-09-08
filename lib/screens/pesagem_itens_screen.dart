9c521cd40b17ce6284e3bd6fabd65b22daea9dc4
boivirtual <boivirtual@agrolandes.com.br>
2026-09-08 16:41:10 -0300

    Ajuste: lib/screens/pesagem_itens_screen.dart

--- lib/screens/pesagem_itens_screen.dart
+++ lib/screens/pesagem_itens_screen.dart
@@ -939,9 +939,6 @@ class _PesagemItensScreenState extends State<PesagemItensScreen> {
 
   void _removerErroOverlay() {
     if (_overlayEntry != null) {
-      debugPrint(
-        "[BUSCA-DEBUG] _removerErroOverlay removendo, vai chamar onClose=${_onOverlayClose != null}",
-      );
       _overlayEntry!.remove();
       _overlayEntry = null;
 

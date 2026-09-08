d039581c56ad9d1bc2d3b108d2432423e01bd652
boivirtual <boivirtual@agrolandes.com.br>
2026-09-08 16:40:50 -0300

    Ajuste: lib/screens/pesagem_itens_screen.dart

--- lib/screens/pesagem_itens_screen.dart
+++ lib/screens/pesagem_itens_screen.dart
@@ -772,9 +772,6 @@ class _PesagemItensScreenState extends State<PesagemItensScreen> {
     VoidCallback? onCancel,
     bool mostrarSimNao = false,
   }) {
-    debugPrint(
-      "[BUSCA-DEBUG] _exibirMensagemOverlay msg='$msg' onClose=${onClose != null} JA_TINHA_OVERLAY=${_overlayEntry != null} onCloseAntigo=${_onOverlayClose != null}",
-    );
     _removerErroOverlay();
     _onOverlayClose = onClose;
 
